cf79a2acd37f01ee827eeecbe2c5942d818ab24c
James Theodore Edward <101777247+jamesedoo@users.noreply.github.com>
2024-01-25 20:36:49 +1100

    done

--- SuperheroApp/app/src/main/java/com/example/superheroapp/HeroesScreen.kt
+++ SuperheroApp/app/src/main/java/com/example/superheroapp/HeroesScreen.kt
@@ -2,47 +2,81 @@ package com.example.superheroapp
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.superheroapp.model.Hero
+import com.example.superheroapp.model.HeroesRepository.heroes
 
 class HeroesScreen {
     @Composable
-    fun HeroItem(modifier: Modifier = Modifier) {
-        Card(modifier = modifier) {
+    fun HeroItem(hero: Hero, modifier: Modifier = Modifier) {
+        Card(
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+            modifier = modifier
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(dimensionResource(id = R.dimen.padding_medium)),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .padding(dimensionResource(id = R.dimen.padding_medium))
+                    .sizeIn(minHeight = 72.dp),
+//                horizontalArrangement = Arrangement.SpaceBetween,
+//                verticalAlignment = Alignment.CenterVertically
             ) {
-                Column {
-                    Text(text = "fdlkafna")
-                    Text(text = "dfklaja")
-                }
-                Image(
-                    painter = painterResource(id = R.drawable.android_superhero1),
-                    contentDescription = stringResource(
-                        id = R.string.description1,
-                    ),
+                Column(
                     modifier = Modifier
+                        .weight(1f)
+//                        .width(250.dp)
+//                        .padding(end = 16.dp)
+//                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(id = hero.nameRes),
+                        modifier = Modifier,
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                    Text(
+                        text = stringResource(id = hero.descriptionRes),
+                        modifier = Modifier,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Box(
+                    modifier = Modifier
+//                        .weight(1f)
                         .size(dimensionResource(id = R.dimen.image_size))
                         .clip(MaterialTheme.shapes.small)
 
-                )
+                ) {
+                    Image(
+                        painter = painterResource(hero.imageRes),
+                        contentDescription = null,
+                        alignment = Alignment.TopCenter,
+                        contentScale = ContentScale.FillWidth
+                    )
+                }
             }
         }
     }
@@ -50,6 +84,6 @@ class HeroesScreen {
     @Preview()
     @Composable
     fun HeroItemPreview() {
-        HeroItem()
+        HeroItem(heroes[1])
     }
 }
